20ff7ee86e0ce86f9a0d2c5552ea32baed99cc2f
yurchenko.s <stas66691@yandex.ru>
2023-01-08 21:26:37 +0400

    feat: add interceptor to work with auth

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -4,28 +4,36 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/base64"
+	"github.com/go-chi/jwtauth/v5"
 	log "github.com/sirupsen/logrus"
 	"github.com/yurchenkosv/credential_storage/internal/credStorageErrors"
 	"github.com/yurchenkosv/credential_storage/internal/model"
 	"github.com/yurchenkosv/credential_storage/internal/repository"
+	"time"
 )
 
 type Auth interface {
 	RegisterUser(ctx context.Context, user *model.User) (*model.User, error)
 	AuthenticateUser(ctx context.Context, user *model.User) (*model.User, error)
+	GenerateToken(user *model.User) (string, error)
+	GetUserFromToken(token string) (*model.User, error)
 }
 
 type AuthService struct {
 	repo repository.Repository
+	auth *jwtauth.JWTAuth
 }
 
-func NewAuthService(repo repository.Repository) Auth {
-	return AuthService{repo: repo}
+func NewAuthService(repo repository.Repository, auth *jwtauth.JWTAuth) *AuthService {
+	return &AuthService{
+		repo: repo,
+		auth: auth,
+	}
 }
 
-func (auth AuthService) RegisterUser(ctx context.Context, user *model.User) (*model.User, error) {
+func (authService *AuthService) RegisterUser(ctx context.Context, user *model.User) (*model.User, error) {
 	user.Password = hashPW(user.Password)
-	savedUser, err := auth.repo.GetUser(ctx, user)
+	savedUser, err := authService.repo.GetUser(ctx, user)
 	if err != nil {
 		log.Error(err)
 		return nil, err
@@ -34,23 +42,54 @@ func (auth AuthService) RegisterUser(ctx context.Context, user *model.User) (*mo
 		err := credStorageErrors.UserAlreadyExistsError{User: user.Username}
 		return nil, &err
 	}
-	err = auth.repo.SaveUser(ctx, user)
+	err = authService.repo.SaveUser(ctx, user)
 	if err != nil {
 		return nil, err
 	}
 
-	savedUser, _ = auth.repo.GetUser(ctx, user)
+	savedUser, _ = authService.repo.GetUser(ctx, user)
 	return savedUser, nil
 }
 
-func (auth AuthService) AuthenticateUser(ctx context.Context, user *model.User) (*model.User, error) {
+func (authService *AuthService) AuthenticateUser(ctx context.Context, user *model.User) (*model.User, error) {
 	user.Password = hashPW(user.Password)
-	userFromRepo, _ := auth.repo.GetUser(ctx, user)
+	userFromRepo, _ := authService.repo.GetUser(ctx, user)
 	if userFromRepo.ID == nil {
 		err := credStorageErrors.InvalidUserError{User: user.Username}
 		return nil, &err
 	}
 	return user, nil
+}
+func (authService *AuthService) GenerateToken(user *model.User) (string, error) {
+	claims := map[string]interface{}{
+		"user_id": *user.ID,
+	}
+	currentTime := time.Now()
+
+	jwtauth.SetIssuedAt(claims, currentTime)
+	jwtauth.SetExpiry(claims, currentTime.Add(5*time.Minute))
+	_, token, err := authService.auth.Encode(claims)
+
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func (authService *AuthService) GetUserFromToken(token string) (*model.User, error) {
+	user := model.User{}
+	jwt, err := jwtauth.VerifyToken(authService.auth, token)
+	if err != nil {
+		return nil, err
+	}
+	id, ok := jwt.Get("user_id")
+	userIDExtracted := id.(float64)
+	userID := int(userIDExtracted)
+	if !ok {
+		return nil, credStorageErrors.InvalidUserError{User: ""}
+	}
+	user.ID = &userID
+	return &user, nil
 }
 
 func hashPW(pw string) string {
